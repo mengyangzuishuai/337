b7a52f55bd7a4d084be29ca548ec50fe2013a959
linhua <18722719931@163.com>
2019-04-01 11:59:42 +0800

    第一次

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class TestThread {
+    public static void main(String[] args) {
+        Threaddemo t1 = new Threaddemo("T1");
+        Threaddemo t2 = new Threaddemo("T2");
+        Threaddemo t21 = new Threaddemo("T2");
+        t1.start();
+        t2.start();
+        t21.start();
+		hellow
+    }
+}
